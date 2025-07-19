8b44586eedc2fc1803cdca48ac00ba39475b3210
Dopamine81 <mohammadjreza81@gmail.com>
2025-07-19 10:37:21 +0330

    Fix

--- 7seg_temperature/7seg_temperature.ino
+++ 7seg_temperature/7seg_temperature.ino
@@ -2,13 +2,13 @@
 DFRobot_SHT20 sht20(&Wire, SHT20_I2C_ADDR);
 const int ledPin = LED_BUILTIN;
 void setup() {
-  // put your setup code here, to run once:
-
+    sht20.initSHT20();
+  delay(100);
+  pinMode(ledPin, OUTPUT);
+  
 }
 
 void loop() {
-  sht20.initSHT20();
-  delay(100);
-  pinMode(ledPin, OUTPUT);
+
 
 }
