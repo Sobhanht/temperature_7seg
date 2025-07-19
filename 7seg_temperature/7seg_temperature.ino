3037999b43cb7ecccef6d3d17199bf54ff6ad8e2
Dopamine81 <mohammadjreza81@gmail.com>
2025-07-19 10:59:19 +0330

    Merging Branch MJR To Main

--- 7seg_temperature/7seg_temperature.ino
+++ 7seg_temperature/7seg_temperature.ino
@@ -1,14 +1,17 @@
 #include "DFRobot_SHT20.h";
 DFRobot_SHT20 sht20(&Wire, SHT20_I2C_ADDR);
 const int ledPin = LED_BUILTIN;
+
 void setup() {
+
   sht20.initSHT20();
   delay(100);
   pinMode(ledPin, OUTPUT);
   
 }
 
-void loop() {
+void loop() {.
+
   float humd = sht20.readHumidity();
   float temp = sht20.readTemperature();
   int temperature = round(temp)
@@ -20,4 +23,5 @@ void loop() {
   Serial.print(" Humidity:");
   Serial.print(humd, 1);
   Serial.print("%");
+  
 }
