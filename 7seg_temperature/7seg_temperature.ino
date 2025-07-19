a3831f22c45e93e8b39889b598fa246876b1e72a
Dopamine81 <mohammadjreza81@gmail.com>
2025-07-19 10:41:40 +0330

    Reading Humidity

--- 7seg_temperature/7seg_temperature.ino
+++ 7seg_temperature/7seg_temperature.ino
@@ -2,13 +2,13 @@
 DFRobot_SHT20 sht20(&Wire, SHT20_I2C_ADDR);
 const int ledPin = LED_BUILTIN;
 void setup() {
-    sht20.initSHT20();
+  sht20.initSHT20();
   delay(100);
   pinMode(ledPin, OUTPUT);
   
 }
 
 void loop() {
-
+  float humd = sht20.readHumidity();
 
 }
